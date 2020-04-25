36a87d369d3aa84f21ec88ce40d8b2efc2f8fae2
alberes <alberes.menezes@gmail.com>
2020-04-25 09:32:34 -0300

    Ajuste no endpoint /categorias/{id}

---
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StardardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StardardError err = new StardardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	
+	}
+}

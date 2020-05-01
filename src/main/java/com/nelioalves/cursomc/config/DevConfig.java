a8aa4ac4d03e703f9510b5229d07cac48105c337
alberes <alberes.menezes@gmail.com>
2020-05-01 20:59:25 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/nelioalves/cursomc/config/DevConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -24,10 +26,15 @@ public class DevConfig {
 	public boolean instantiateDataBase() throws ParseException {
 
 		if (!"create".equals(strategy)) {
-			return false;
+			//return false;
 		}
 
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,27 @@
+package com.nelioalves.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmtpEmailService extends AbstractEmailService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Autowired
+	private MailSender mailSender;
+	
+	@Override
+	public void sendEmail(SimpleMailMessage message) {
+		LOG.info("Enviando de email");
+		mailSender.send(message);
+		LOG.info(message.toString());
+		LOG.info("Email enviado");
+
+	}
+
+}

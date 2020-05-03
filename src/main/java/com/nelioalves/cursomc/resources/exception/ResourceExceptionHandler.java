735f9b25f5601e0a298c1144a3c37217e2c49dc7
alberes <alberes.menezes@gmail.com>
2020-05-03 12:03:46 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.services.exception.AuthorizationException;
 import com.nelioalves.cursomc.services.exception.DataIntegrityException;
 import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
 
@@ -43,6 +44,15 @@ public class ResourceExceptionHandler {
 		}
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	
+	}
+	
+	@ExceptionHandler(AuthorizationException.class)
+	public ResponseEntity<StardardError> authorization(AuthorizationException e, HttpServletRequest request){
+		
+		StardardError err = new StardardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	
 	}
 }
---
+++ src/main/java/com/nelioalves/cursomc/services/UserService.java
@@ -0,0 +1,20 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.security.UserSS;
+
+@Service
+public class UserService {
+
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+}

858df097cd2c72938f162564b1f1da7cc22cc618
alberes <alberes.menezes@gmail.com>
2020-05-03 13:06:40 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/nelioalves/cursomc/services/exception/AuthorizationException.java
+++ src/main/java/com/nelioalves/cursomc/services/exception/AuthorizationException.java
@@ -2,7 +2,7 @@ package com.nelioalves.cursomc.services.exception;
 
 public class AuthorizationException extends RuntimeException{
 
-	private static final long serialVersionUID = 2697418866384006091L;
+	private static final long serialVersionUID = -5522390524720046893L;
 
 	public AuthorizationException(String msg) {
 		super(msg);
